6fe563e53c678a1e99934388cb6c3dbb180db585
vladislav-i <saintmz3@gmail.com>
2015-06-25 15:38:38 -0700

    [refactor] Added website info

--- client/js/javascript.js
+++ client/js/javascript.js
@@ -1,4 +1,4 @@
-angular.module('website', ['ngRoute'])
+angular.module('website', ['ngRoute', 'easypiechart', 'ngMap'])
   .config(function($routeProvider) {
     $routeProvider
     // defined four routes
@@ -8,5 +8,29 @@ angular.module('website', ['ngRoute'])
       .when('/contact', {templateUrl:'partials/contact.html'})
       .otherwise({redirectTo: '/home', template:'partials/home.html'});
   });
+
 function MainCtrl($scope) {
+  $scope.javascript="93%";
+  $scope.jquery="73%";
+  $scope.css="95%";
+  $scope.angularjs ="87%";
+  $scope.options = {
+    animate:{
+        duration:1000,
+        enabled:true
+    },
+    barColor:'#0085d1',
+    scaleColor:true,
+    lineWidth:4,
+    lineCap:'round',
+    size: '120'
+  };
+
+  // contactform
+  $scope.sendMessage = function() {
+    var contactName;
+    console.log("here");
+    var contactEmail;
+    var contactMessage;
+  }
 }
